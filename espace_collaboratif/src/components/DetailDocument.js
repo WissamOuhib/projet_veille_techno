2199c8fde6fd60fa55ba2f294f832e1310764688
wissem <wissem.ouhib@gmail.com>
2022-10-08 01:46:21 +0200

    autre css

--- espace_collaboratif/src/components/DetailDocument.js
+++ espace_collaboratif/src/components/DetailDocument.js
@@ -1,19 +1,15 @@
 import { useState } from "react";
 
+import '.././potato.css';
+
 export default function DetailDocument() {
 
     return ( 
     <div>
-        <h1>Détail document</h1>
+        <h1 className="potato">Détail document</h1>
 
-        <tr>
-            <td>
             <button>Sauvegarder</button>
-            </td>
-            <td>
             <button>Proposer solution</button>
-            </td>            
-        </tr>
 
         {/* <iframe 
         src="http://infolab.stanford.edu/pub/papers/google.pdf#toolbar=0&navpanes=0&scrollbar=0" 
